9b0a1f0400015fa0ec349781cd87a0159745fd16
giao@lango <giao@etancesys.com>
2019-11-18 12:34:53 +0100

    React scaffolding

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<div>
+		<h1>Testing</h1>
+		<p>this is a paragraph</p>
+		<a href="#">Click me</a>
+	</div>,
+	document.querySelector('#root'),
+)
